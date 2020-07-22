498711fb1513a122467e1a6b42b5be731fca7cd1
youngjjjj <>
2020-07-22 21:46:25 +0900

    FIX params to search & load people_list from excel files

--- bill.py
+++ bill.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import unquote
 import requests
 import json
-from people_list import people
 
 with open("secret.json") as f:
     secret = json.loads(f.read())
@@ -21,7 +20,9 @@ excel_file = openpyxl.load_workbook(
     "국회의원 입법활동 조사 명단(대표발의).xlsx"
 )
 
-sheet = excel_file["결과"]
+sheet_person = excel_file["명단"]
+
+sheet_result = excel_file["결과"]
 
 API_KEY = get_secret("API_KEY")
 
@@ -29,9 +30,10 @@ url = "http://apis.data.go.kr/9710000/BillInfoService2/getBillInfoList"
 
 decode_API_KEY = unquote(API_KEY)
 
-people_list = people.split("\n")
+people_list = [row[1].value for row in sheet_person.rows]
 
 idx = 1
+count = 1
 
 for person in people_list:
     params = {
@@ -50,8 +52,6 @@ for person in people_list:
         "end_propose_num": "-",
         "proposer_kind_cd": "F01",
         "gbn": "dae_num_name",
-        "amendmentyn": "false",
-        "budget": "false",
     }
 
     response = requests.get(url, params=params)
@@ -59,29 +59,32 @@ for person in people_list:
     content = soup.find("response")
     items = content.find("items")
 
+    sheet_person.cell(row=count, column=3).value = len(items)
+    count += 1
+
     for item in items:
         # 의원명
-        sheet.cell(row=idx + 1, column=1).value = person
+        sheet_result.cell(row=idx + 1, column=1).value = person
         # 의안명
-        sheet.cell(row=idx + 1, column=2).value = (
+        sheet_result.cell(row=idx + 1, column=2).value = (
             item.find("billname").text if not item.find("billname") is None else ""
         )
         # 제안일자
-        sheet.cell(row=idx + 1, column=3).value = (
+        sheet_result.cell(row=idx + 1, column=3).value = (
             item.find("proposedt").text if not item.find("proposedt") is None else ""
         )
         # 의결결과
-        sheet.cell(row=idx + 1, column=4).value = (
+        sheet_result.cell(row=idx + 1, column=4).value = (
             item.find("generalresult").text
             if not item.find("generalresult") is None
             else ""
         )
         # 주요내용
-        sheet.cell(row=idx + 1, column=5).value = (
+        sheet_result.cell(row=idx + 1, column=5).value = (
             item.find("summary").text if not item.find("summary") is None else ""
         )
         # 심사진행 상태
-        sheet.cell(row=idx + 1, column=6).value = (
+        sheet_result.cell(row=idx + 1, column=6).value = (
             item.find("procstagecd").text
             if not item.find("procstagecd") is None
             else ""
@@ -92,28 +95,3 @@ for person in people_list:
 
 excel_file.save(filename="국회의원 입법활동 조사 명단(대표발의).xlsx")
 print("Complete!")
-
-
-#     # 의안명
-#     bill_name = [billname.text for billname in content.find_all("billname")]
-#     # 제안일자
-#     date = [proposedt.text for proposedt in content.find_all("proposedt")]
-#     # 의결결과
-#     result = [generalresult.text for generalresult in content.find_all("generalresult")]
-#     # 주요내용
-#     summary = [summary.text for summary in content.find_all("summary")]
-#     # 심사진행상태
-#     process = [procstagecd.text for procstagecd in content.find_all("procstagecd")]
-#
-#     for data in zip(bill_name, date, result, summary, process):
-#         sheet.cell(row=idx + 1, column=1).value = person
-#         sheet.cell(row=idx + 1, column=2).value = data[0]
-#         sheet.cell(row=idx + 1, column=3).value = data[1]
-#         sheet.cell(row=idx + 1, column=4).value = data[2]
-#         sheet.cell(row=idx + 1, column=5).value = data[3]
-#         sheet.cell(row=idx + 1, column=6).value = data[4]
-#         idx += 1
-#     idx += 1
-#
-# excel_file.save(filename="국회의원 입법활동 조사 명단(대표발의).xlsx")
-# print("Complete!")
